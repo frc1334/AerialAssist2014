fd5d187c13b332eeb1ca6ca8820fdb165a4ec2bb
banana456 <andrew.aling@gmail.com>
2014-01-10 17:27:13 -0500

    created the tank and arcade systems to the drive trains

--- Subsystems/DriveTrainSubsystem.cpp
+++ Subsystems/DriveTrainSubsystem.cpp
@@ -2,14 +2,26 @@
 #include "../Robotmap.h"
 
 DriveTrainSubsystem::DriveTrainSubsystem() : Subsystem("DriveTrainSubsystem") {
-	
+
 }
-    
+
 void DriveTrainSubsystem::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
 }
 
+void DriveTrainSubsystem::tankDrive(float left, float right)
+{
+    left1.Set(-(left));
+    left2.Set(-(left));
+    right1.Set(right);
+    right2.Set(right);
+}
 
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
+void DriveTrainSubsystem::arcadeDrive(float drive, float turn)
+{
+    left1.Set(-(drive - turn));
+	left2.Set(-(drive - turn));
+	right1.Set(drive + turn);
+	right2.Set(drive + turn);
+}
--- Subsystems/DriveTrainSubsystem.h
+++ Subsystems/DriveTrainSubsystem.h
@@ -12,9 +12,18 @@ class DriveTrainSubsystem: public Subsystem {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
+
+	Jaguar left1;
+	Jaguar left2;
+	Jaguar right1;
+	Jaguar right2;
+
 public:
 	DriveTrainSubsystem();
 	void InitDefaultCommand();
+
+	void tankDrive(float left, float right);
+	void arcadeDrive(float drive, float turn);
 };
 
 #endif
