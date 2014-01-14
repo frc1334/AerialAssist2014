d9a57cbff8b24b5d52aee8e795cf581c1df7ab11
josephd <joe@w00t42.net>
2014-01-13 20:09:31 -0500

    Removed example code

--- CommandBasedRobot.cpp
+++ CommandBasedRobot.cpp
@@ -1,21 +1,17 @@
 #include "WPILib.h"
 #include "Commands/Command.h"
-#include "Commands/ExampleCommand.h"
 #include "CommandBase.h"
 
 class CommandBasedRobot : public IterativeRobot {
 private:
-	Command *autonomousCommand;
 	LiveWindow *lw;
 	
 	virtual void RobotInit() {
 		CommandBase::init();
-		autonomousCommand = new ExampleCommand();
 		lw = LiveWindow::GetInstance();
 	}
 	
 	virtual void AutonomousInit() {
-		autonomousCommand->Start();
 	}
 	
 	virtual void AutonomousPeriodic() {
@@ -23,11 +19,6 @@ private:
 	}
 	
 	virtual void TeleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to 
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		autonomousCommand->Cancel();
 	}
 	
 	virtual void TeleopPeriodic() {
