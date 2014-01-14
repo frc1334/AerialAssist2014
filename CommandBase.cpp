66d4539e171bbc040c015bb233d921f1dfa872fb
josephd <joe@w00t42.net>
2014-01-13 20:05:04 -0500

    Removed example subsystem instantiation

--- CommandBase.cpp
+++ CommandBase.cpp
@@ -1,5 +1,4 @@
 #include "CommandBase.h"
-#include "Subsystems/ExampleSubsystem.h"
 #include "Commands/Scheduler.h"
 
 CommandBase::CommandBase(const char *name) : Command(name) {
@@ -9,13 +8,13 @@ CommandBase::CommandBase() : Command() {
 }
 
 // Initialize a single static instance of all of your subsystems to NULL
-ExampleSubsystem* CommandBase::examplesubsystem = NULL;
+//ExampleSubsystem* CommandBase::examplesubsystem = NULL;
 OI* CommandBase::oi = NULL;
 
 void CommandBase::init() {
     // Create a single static instance of all of your subsystems. The following
 	// line should be repeated for each subsystem in the project.
-	examplesubsystem = new ExampleSubsystem();
+	//examplesubsystem = new ExampleSubsystem();
 	
 	oi = new OI();
 }
