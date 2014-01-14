3eaedcc461e1ba4141ec434b3a57a82a6f4073e4
josephd <joe@w00t42.net>
2014-01-13 20:04:20 -0500

    Removed example subsystem

--- CommandBase.h
+++ CommandBase.h
@@ -2,7 +2,6 @@
 #define COMMAND_BASE_H
 
 #include "Commands/Command.h"
-#include "Subsystems/ExampleSubsystem.h"
 #include "OI.h"
 
 
@@ -17,7 +16,7 @@ public:
 	CommandBase();
 	static void init();
 	// Create a single static instance of all of your subsystems
-	static ExampleSubsystem *examplesubsystem;
+	//static ExampleSubsystem *examplesubsystem;
 	static OI *oi;
 };
 
