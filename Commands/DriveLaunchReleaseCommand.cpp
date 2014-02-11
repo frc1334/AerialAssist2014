7d751e0d3c097d92c5f2cf2f1ad7e847ba93465d
Joseph Dillon <joe@w00t42.net>
2014-02-11 16:42:32 -0500

    launch command skeleton

---
+++ Commands/DriveLaunchReleaseCommand.cpp
@@ -0,0 +1,31 @@
+#include "DriveLaunchReleaseCommand.h"
+
+DriveLaunchReleaseCommand::DriveLaunchReleaseCommand() {
+	// Use requires() here to declare subsystem dependencies
+	// eg. requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void DriveLaunchReleaseCommand::Initialize() {
+	
+}
+
+// Called repeatedly when this Command is scheduled to run
+void DriveLaunchReleaseCommand::Execute() {
+	
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool DriveLaunchReleaseCommand::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void DriveLaunchReleaseCommand::End() {
+	
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void DriveLaunchReleaseCommand::Interrupted() {
+}
---
+++ Commands/LaunchCommandGroup.h
@@ -0,0 +1,16 @@
+#ifndef LAUNCHCOMMANDGROUP_H
+#define LAUNCHCOMMANDGROUP_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author p
+ */
+class LaunchCommandGroup: public CommandGroup {
+public:	
+	LaunchCommandGroup();
+};
+
+#endif
