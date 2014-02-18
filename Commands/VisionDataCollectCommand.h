b5e531b50937ea31df0a5e2f0ce2fe2e755ebd71
Megapead <gabrielciolac@gmail.com>
2014-02-18 15:47:19 -0500

    Added information to VisionDataCollectCommand.h

--- Commands/VisionDataCollectCommand.h
+++ Commands/VisionDataCollectCommand.h
@@ -4,6 +4,15 @@
 #include "../CommandBase.h"
 #include "../AutonomousTarget.h"
 
+/*
+**************************************************************************************
+ The Vision collect subsystem features a few critical function for "discovering" the
+hot goal. The "magic" happens in the "Execute" feild. In this feild the camera starts
+collecting data from the camera and then you must define a target (Left or Right)
+you will then set a timer which will analyze the information given for a set time.
+**************************************************************************************
+*/
+
 class VisionDataCollectCommand: public CommandBase
 {
 private:
