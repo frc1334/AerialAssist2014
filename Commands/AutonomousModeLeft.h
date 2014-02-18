180636c71015ef70525a0c9e15242de44825e306
Megapead <gabrielciolac@gmail.com>
2014-02-18 16:08:25 -0500

    Added a description in the AutonomousMode Left and Right headers

--- Commands/AutonomousModeLeft.h
+++ Commands/AutonomousModeLeft.h
@@ -1,6 +1,12 @@
 #ifndef AUTONOMOUSMODELEFT_H
 #define AUTONOMOUSMODELEFT_H
 
+/**
+**********************************************************************************************
+This command runs the routine for the left mode of the autonomous mode.
+**********************************************************************************************
+*/
+
 #include "Commands/CommandGroup.h"
 
 /**
