7ffc10eb5b087f3399c2129ef70178705de440b9
Ben Hosein <BenHosein713@gmail.com>
2014-02-18 16:29:34 -0500

    added the documentation to the target switch command

--- Commands/TargetSwitchCommand.h
+++ Commands/TargetSwitchCommand.h
@@ -4,6 +4,14 @@
 #include "../CommandBase.h"
 #include "../AutonomousTarget.h"
 
+/**
+***************************************************************************
+It requires the vision subsystem, then it saves the variables of the left 
+or right target, then depending on what target is "hot" , it will go back
+to that variable (left or right) and pick the hot one, and run that command
+***************************************************************************
+*/
+
 class TargetSwitchCommand: public CommandBase
 {
 private:
