44e0177b017a169a7e3eab44c6e1a07d4b6d99eb
Megapead <gabrielciolac@gmail.com>
2014-03-01 17:32:07 -0500

    Finished AutonomousModeLeft,not tested

--- Commands/AutonomousModeLeft.cpp
+++ Commands/AutonomousModeLeft.cpp
@@ -24,13 +24,14 @@ AutonomousModeLeft::AutonomousModeLeft()
 // arm.
 //Note for AutonomousDriveCommand First float is equal to motor, second is equal to forward, third
 //is equal to time
-  //AddSequential (new VisionDataCollectCommand(Left));
-  AddSequential (new AutonomousDriveCommand(0.0,0.3,0.5));
-  AddSequential (new AutonomousDriveCommand(0.0,-0.5,0.5));
-  //AddSequential (new VisionDataCollectCommand(Right));
-  //AddSequential (new TargetSwitchCommand(new AutonomousDriveCommand (0.0,0.3,0.5),new AutonomousDriveCommand(0.0,-0.3,0.5)));
-  //AddSequential (new LaunchCommandGroup ());
-  AddSequential (new AutonomousDriveCommand(1.0,0.0,1.8));
+  AddSequential (new WinchRewindCommand());
+  AddSequential (new VisionDataCollectCommand(Left));
+  AddSequential (new AutonomousDriveCommand(0.0,0.3,1.0));
+  AddSequential (new VisionDataCollectCommand(Right));
+  AddSequential (new AutonomousDriveCommand(0.0,-0.3,1.0));
+  AddSequential (new TargetSwitchCommand(new AutonomousDriveCommand (0.0,0.3,1.0),new AutonomousDriveCommand(0.0,-0.3,1.0)));
+  AddSequential (new LaunchCommandGroup ());
+  AddSequential (new AutonomousDriveCommand(1.0,0.0,0.8));
 
 
 }
