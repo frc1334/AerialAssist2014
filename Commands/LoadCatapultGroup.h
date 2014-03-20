b282ce1151e836cc0e2612d130cc10fd794f3617
josephd <joe@w00t42.net>
2014-03-20 05:58:51 -0400

    Ended comment

--- Commands/LoadCatapultGroup.h
+++ Commands/LoadCatapultGroup.h
@@ -10,9 +10,7 @@ the "claws" and the wheels attached, loading the robot
 requires it to be in the low position unless it is catching
 the ball
 *************************************************************
-/**
-
-
+**/
 
 class LoadCatapultGroup: public CommandGroup
 {
