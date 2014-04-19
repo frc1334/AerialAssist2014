ebfc280da535a5deaed4501e87b91ac55367926e
josephd <joe@w00t42.net>
2014-04-19 15:07:35 -0400

    Finished auto modes

--- Commands/AutonomousModeLowGoal.cpp
+++ Commands/AutonomousModeLowGoal.cpp
@@ -1,6 +1,28 @@
 #include "AutonomousModeLowGoal.h"
+#include "LoadCatapultGroup.h"
+#include "Commands/WaitCommand.h"
+#include "OperatorLowCommand.h"
+#include "OperatorHighCommand.h"
+#include "OperatorPickup1Command.h"
+#include "OperatorPickup2Command.h"
+#include "AutonomousPickupCommand.h"
+#include "AutonomousDriveCommand.h"
+#include "AutonomousCoastCommand.h"
+#include "AutonomousLowGearCommand.h"
+#include "DriveLaunchReleaseCommand.h"
+#include "../Timing.h"
 
 AutonomousModeLowGoal::AutonomousModeLowGoal()
 {
-  // see OneBallCommandGroup.cpp
+  AddSequential(new AutonomousLowGearCommand());
+  AddSequential(new LoadCatapultGroup());
+  AddSequential(new OperatorLowCommand());
+  AddSequential(new AutonomousPickupCommand(2.0));
+  AddSequential(new OperatorPickup2Command());
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
+  AddParallel(new OperatorPickup2Command(0.5));
+  AddSequential(new AutonomousCoastCommand(1.0f, TURN_CORRECTION, 0.0f, TURN_CORRECTION, 0.5));
+  AddParallel(new OperatorPassCommand()); // run with the timing command
+  AddSequential(new WaitCommand(1.0));
+  AddSequential(new OperatorHighCommand()); // cancel the pass
 }
--- Commands/AutonomousModeRamGoal.cpp
+++ Commands/AutonomousModeRamGoal.cpp
@@ -1,21 +1,26 @@
 #include "AutonomousModeRamGoal.h"
+#include "LoadCatapultGroup.h"
+#include "Commands/WaitCommand.h"
+#include "OperatorLowCommand.h"
+#include "OperatorHighCommand.h"
+#include "OperatorPickup1Command.h"
+#include "OperatorPickup2Command.h"
+#include "AutonomousPickupCommand.h"
+#include "AutonomousDriveCommand.h"
+#include "AutonomousCoastCommand.h"
+#include "AutonomousLowGearCommand.h"
+#include "DriveLaunchReleaseCommand.h"
+#include "../Timing.h"
 
 AutonomousModeRamGoal::AutonomousModeRamGoal()
 {
-  // Add Commands here:
-  // e.g. AddSequential(new Command1());
-  //      AddSequential(new Command2());
-  // these will run in order.
-
-  // To run multiple commands at the same time,
-  // use AddParallel()
-  // e.g. AddParallel(new Command1());
-  //      AddSequential(new Command2());
-  // Command1 and Command2 will run in parallel.
-
-  // A command group will require all of the subsystems that each member
-  // would require.
-  // e.g. if Command1 requires chassis, and Command2 requires arm,
-  // a CommandGroup containing them would require both the chassis and the
-  // arm.
+  AddSequential(new AutonomousLowGearCommand());
+  AddSequential(new LoadCatapultGroup());
+  AddSequential(new OperatorLowCommand());
+  AddSequential(new AutonomousPickupCommand(2.0));
+  AddSequential(new OperatorPickup2Command());
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.5));
+  AddParallel(new OperatorPassCommand()); // run with the timing command
+  AddSequential(new WaitCommand(1.0));
+  AddSequential(new OperatorHighCommand()); // cancel the pass
 }
